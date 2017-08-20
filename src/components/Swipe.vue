d4de8ad991c02dde5bfc9abaf8ebf623dbc05e8d
abc02 <abcqq02@qq.com>
2017-08-20 23:27:22 +0800

    npm install swiper & Swipe.vue

---
+++ src/components/Swipe.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swp-page">
+                <a class="js-no-follow" href="https://h5.koudaitong.com/v2/index/rukou">
+                    <img class="goods-main-photo fadeIn" src="https://img.yzcdn.cn/upload_files/2016/07/29/Fl3T06Mu7TpIhR4L1s2tzm8cZrgt.jpg">
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,14 +1,14 @@
 let url = {
-    hotLists: 'index/hotLists'
+    hotLists: '/index/hotLists'
 }
 
 
 // 开发环境和金额真实环境切换
 // let host = ''
-let host ='http://rapapi.org/mockjsdata/23334/'
+let host = 'http://rapapi.org/mockjsdata/24170'
 
-for (let key in url){
-    if(url.hasOwnProperty(key)){
+for (let key in url) {
+    if (url.hasOwnProperty(key)) {
         url[key] = host + url[key]
     }
 }
