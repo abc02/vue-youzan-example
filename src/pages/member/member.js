bbe201aa632bd6e0b0b73d3c79f7b4b4c2efbc0a
abc02 <abcqq02@qq.com>
2017-09-11 21:37:34 +0800

    add router & store

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,31 +1,5 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-Vue.use(Router)
-
-let routes = [{
-    path: '/',
-    component: require('./components/member.vue')
-}, {
-    path: '/address',
-    component: require('./components/address.vue'),
-    children: [
-        {
-            path: '',
-            component: require('./components/all.vue')
-        }, {
-            path: 'all',
-            name: 'all', // 命名路由
-            component: require('./components/all.vue')
-        }, {
-            path: 'form',
-            name: 'form', 
-            component: require('./components/form.vue')
-        }]
-}]
-// 创建Router实例
-let router = new Router({
-    routes,
-})
+import router from './router/index.js'
 
 //根组件注入
 new Vue({
---
+++ src/pages/member/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+let routes = [{
+    path: '/',
+    component: require('../components/member.vue')
+}, {
+    path: '/address',
+    component: require('../components/address.vue'),
+    children: [
+        {
+            path: '',
+            component: require('../components/all.vue')
+        }, {
+            path: 'all',
+            name: 'all', // 命名路由
+            component: require('../components/all.vue')
+        }, {
+            path: 'form',
+            name: 'form', 
+            component: require('../components/form.vue')
+        }]
+}]
+// 创建Router实例
+let router = new Router({
+    routes,
+})
+
+
+export default router
